54508006ca79fa058a5bba2a6359423b4c6d63b2
zhaojiyu418haha-maker <zhaojiyu418haha@gmail.com>
2026-01-13 17:02:23 +0300

    Add files via upload

---
+++ дз0часть2.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Lesson_1
+{
+    class Pro
+    {
+        static void sanheer()
+        {
+            Console.WriteLine("пишите число");
+            string a = Console.ReadLine();
+            int b = int.Parse(a);
+            for (int c = 1; c <= b; c++)
+            {
+                int d = c % 2;
+                int e = c % 3;
+                if (d == 0 || e == 0)
+                {
+                    Console.WriteLine(c);
+                }
+            }
+        }
+        static void caishu()
+        {
+            Console.WriteLine("угадайте  число");
+            int d = 7;
+
+            for (int c = 0; c <= 999; c++)
+            {
+                string a = Console.ReadLine();
+                int b = int.Parse(a);
+                if (b > d)
+                {
+                    Console.WriteLine("больше");
+
+                }
+                if (b < d)
+                {
+                    Console.WriteLine("меньше");
+
+                }
+                if (b == d)
+                {
+                    Console.WriteLine("верно");
+                }
+            }
+
+
+
+
+        }
+        static void Main()
+        {
+            sanheer();
+            caishu();
+        }
+    }
+}
