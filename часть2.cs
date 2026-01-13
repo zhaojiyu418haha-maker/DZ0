d7c9da831d8d7567654804e0936a0473372b0802
zhaojiyu418haha-maker <zhaojiyu418haha@gmail.com>
2026-01-13 16:56:17 +0300

    Add files via upload

---
+++ часть2.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+
+namespace Lesson_1
+{
+    class Pro
+    {
+        static void mima()
+        {
+            Console.WriteLine("пишите  ваш  пароль");
+
+            int d = 12345;
+
+            for(int c = 0; c <= 999; c++)
+            {
+                string a = Console.ReadLine();
+                int b = int.Parse(a);
+                if (b == d) 
+                {
+                    Console.WriteLine("верно");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("no верно");
+                }
+            }
+            
+        }
+        static void Main()
+        {
+            mima();
+        }
+    }
+}
